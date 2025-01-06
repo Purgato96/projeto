abcf5d6e6e8ad6246fcf7459e95e376e7a484820
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-06 20:36:22 -0300

    Criando rotas, controladores, viewers do site

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('home');
-});
+});*/
+
+/** 
+ * Rotas Admin */
+
+
+/**
+ * Rotas Site
+ */
+
+Route::get('/', [SiteController::class, 'index'])->name('site.home');
+
+ /**
+  * Rotas User
+  */
