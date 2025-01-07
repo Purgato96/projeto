dcb04880060ecc71d23ea4b320d237b70de7e83f
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-07 17:04:35 -0300

    Construindo a Blade Master, Site e Post Controllers

---
+++ resources/views/site/footer.blade.php
@@ -0,0 +1,6 @@
+@extends(master.blade.php)
+@section('footer')
+    <footer>
+        <h1>Footer</h1>
+    </footer>
+@endsection
---
+++ resources/views/site/header.blade.php
@@ -0,0 +1,6 @@
+@extends(master.blade.php)
+@section('header')
+    <header>
+        <h1>Header</h1>
+    </header>
+@endsection
